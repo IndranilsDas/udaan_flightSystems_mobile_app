0c82f87c2cee78a09234f4325f8d00f0293998ea
Indranil Das <indranildas169@gmail.com>
2025-07-31 11:53:41 +0530

    minor

--- app/(tabs)/forms/page.tsx
+++ app/(tabs)/forms/page.tsx
@@ -13,11 +13,11 @@ export default function page() {
       <View className='flex flex-col w-5/6 h-screen justify-center items-center gap-4'>
       <Text className='text-3xl font-semibold'>START FLIGHT</Text>
       <View className='flex flex-col justify-center h-3/5 w-full gap-3'>
-        <TextInput placeholder='select Project' className='border-b-2 w-full'/>
-        <TextInput placeholder='Employee ID :' className='border-b-2 w-full'/>
-        <TextInput placeholder='Drone ID :' className='border-b-2 w-full'/>
-        <TextInput placeholder='Battery ID :' className='border-b-2 w-full'/>
-        <TextInput placeholder='Start Voltage in Volts :' className='border-b-2 w-full'/>
+        <TextInput value={} onChangeText={} placeholder='select Project' className='border-b-2 w-full'/>
+        <TextInput value={empID} onChangeText={setEmpID} placeholder='Employee ID :' className='border-b-2 w-full'/>
+        <TextInput value={droneID} onChangeText={setDroneID} placeholder='Drone ID :' className='border-b-2 w-full'/>
+        <TextInput value={batteryID} onChangeText={setBatteryID} placeholder='Battery ID :' className='border-b-2 w-full'/>
+        <TextInput value={startVoltage} onChangeText={setStartVoltage} placeholder='Start Voltage in Volts :' className='border-b-2 w-full'/>
       </View>
       <TouchableOpacity className='p-3 w-3/4 border rounded-full items-center'>
         <Text>Submit</Text>
